fbb285d6903189ed2e9d2a1630ed4588a8af1099
fireflieslive <lnabong@outlook.com>
2016-03-13 18:19:13 +0800

    initial commit

---
+++ src/main/java/com/fireflieslive/zelbewiser/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.fireflieslive.zelbewiser.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String welcome( Model model ) {
+		model.addAttribute( "greeting", "Welcome to zelbewiser app!" );
+		model.addAttribute( "tagline", "The one and only amazing webstore" );
+		
+		return "welcome";
+	}
+}
