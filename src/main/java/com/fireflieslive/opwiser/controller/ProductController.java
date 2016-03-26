d4771cd2f236e4fc0544913fe3c2374e452494be
fireflieslive <lnabong@outlook.com>
2016-03-26 15:07:55 +0800

    P157

--- src/main/java/com/fireflieslive/opwiser/controller/ProductController.java
+++ src/main/java/com/fireflieslive/opwiser/controller/ProductController.java
@@ -1,26 +1,21 @@
 package com.fireflieslive.opwiser.controller;
 
-import java.math.BigDecimal;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.fireflieslive.opwiser.domain.Product;
+import com.fireflieslive.opwiser.domain.repository.ProductRepository;
 
 @Controller
 public class ProductController {
 
-	@RequestMapping( "/products" )
-	public String list( Model model ) {
-		Product iphone = new Product( "P1234", "Iphone 5s", new BigDecimal( 500 ));
-		iphone.setDescription( "Apple iPhone 5s smartphone with 4.00-inch 640X390 display and 8-megapixel rear camera" );
-		iphone.setCategory( "Smart Phone" );
-		iphone.setManufacturer( "Apple" );
-		iphone.setUnitsInStock( 1000 );
-		
-		model.addAttribute( "product", iphone );
-		
+	@Autowired
+	private ProductRepository productRepository;
+
+	@RequestMapping("/products")
+	public String list(Model model) {
+		model.addAttribute("products", productRepository.getAllProducts());
 		return "products";
 	}
 }
---
+++ src/main/java/com/fireflieslive/opwiser/domain/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.fireflieslive.opwiser.domain.repository;
+
+import java.util.List;
+
+import com.fireflieslive.opwiser.domain.Product;
+
+public interface ProductRepository {
+	List<Product> getAllProducts();
+	
+}
